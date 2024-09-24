f492817b9c27062ff444894501bbe2cabcdaca36
Adriana Bamberger <162661198+Adriana-Bamberger@users.noreply.github.com>
2024-09-24 22:40:59 +1200

    First style pokemon card

--- client/components/PokemonCard.tsx
+++ client/components/PokemonCard.tsx
@@ -3,12 +3,14 @@ import { Type, PokemonCardProps } from './types';
 
 const PokemonCard: React.FC<PokemonCardProps> = ({ pokemon }) => {
   return (
-    <div>
-      <h2>{pokemon.name}</h2>
-      <p>ID: {pokemon.id}</p>
-      <p>Types: {pokemon.types.map((type: Type) => type.type.name).join(', ')}</p>
-      <p>Height: {pokemon.height} decimetres</p>
-      <p>Weight: {pokemon.weight} hectograms</p>
+    <div 
+    className="bg-white rounded-lg shadow-md p-4 w-64 h-64 cursor-pointer"
+    onClick={onClick} >
+      <h2 className="text-lg font-bold">{pokemon.name}</h2>
+      <p className="text-sm text-gray-600">ID: {pokemon.id}</p>
+      <p className="text-sm text-gray-600">Types: {pokemon.types.map((type: Type) => type.type.name).join(', ')}</p>
+      <p className="text-sm text-gray-600">Height: {pokemon.height} decimetres</p>
+      <p className="text-sm text-gray-600">Weight: {pokemon.weight} hectograms</p>
     </div>
   );
 };
