80e53b9c2197d2374165d81c4e8a682a52edae21
Adriana Bamberger <162661198+Adriana-Bamberger@users.noreply.github.com>
2024-09-24 22:06:02 +1200

    Adjust types and Interfaces for current use

--- client/components/types.ts
+++ client/components/types.ts
@@ -2,11 +2,23 @@
 export interface Pokemon {
   id: number;
   name: string;
-  types: { type: { name: string } }[];
+  types: Type[];
   height: number;
   weight: number;
   sprites: {
     front_default: string;
   };
   front_default: string;
+}
+
+export interface Type {
+  type: { name: string };
+}
+
+export interface PokemonCardProps {
+  pokemon: Pokemon;
+}
+
+export interface PokemonResult {
+  url: string;
 }
