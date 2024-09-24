a6b5323ca2ecfe099e47b54a0deb7f2c05bc95bb
Adriana Bamberger <162661198+Adriana-Bamberger@users.noreply.github.com>
2024-09-24 20:56:55 +1200

    Adjust set ups
    Boilerplate needed adjusting

--- client/index.tsx
+++ client/index.tsx
@@ -1,8 +1,10 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React from 'react';
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
-import App from './components/App.tsx'
+import App from './components/App'
 
 const queryClient = new QueryClient()
 
